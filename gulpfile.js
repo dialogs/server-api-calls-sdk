21e22b6e9680ea0482b4a39706af7881cc178514
Izmailskiy <imd@dlg.im>
2019-07-24 21:27:09 +0300

    chore: ice

--- gulpfile.js
+++ gulpfile.js
@@ -66,15 +66,18 @@ gulp.task ('grpc-replace-enum', ['grpc-generate'], function () {
         .pipe(gulpReplace("import { Observable } from 'rxjs/Observable';", "import { Observable } from 'rxjs/Rx';"))
 
         
+        .pipe(gulpReplace('CALL_DIRECTION_UNKNOWN = 0', 'CALL_DIRECTION_UNKNOWN = "CALL_DIRECTION_UNKNOWN"'))
         .pipe(gulpReplace('CALL_DIRECTION_INBOUND = 1', 'CALL_DIRECTION_INBOUND = "CALL_DIRECTION_INBOUND"'))
         .pipe(gulpReplace('CALL_DIRECTION_OUTBOUND = 2', 'CALL_DIRECTION_OUTBOUND = "CALL_DIRECTION_OUTBOUND"'))
 
+        .pipe(gulpReplace('CALL_STATE_UNKNOWN = 0', 'CALL_STATE_UNKNOWN = "CALL_STATE_UNKNOWN"'))
         .pipe(gulpReplace('CALL_STATE_TRIYNG = 1', 'CALL_STATE_TRIYNG = "CALL_STATE_TRIYNG"'))
         .pipe(gulpReplace('CALL_STATE_PROGRESS = 2', 'CALL_STATE_PROGRESS = "CALL_STATE_PROGRESS"'))
         .pipe(gulpReplace('CALL_STATE_RINGING = 3', 'CALL_STATE_RINGING = "CALL_STATE_RINGING"'))
         .pipe(gulpReplace('CALL_STATE_TALKING = 4', 'CALL_STATE_TALKING = "CALL_STATE_TALKING"'))
         .pipe(gulpReplace('CALL_STATE_FINISHED = 5', 'CALL_STATE_FINISHED = "CALL_STATE_FINISHED"'))
 
+        .pipe(gulpReplace('DISPOSE_REASON_UNKNOWN = 0', 'DISPOSE_REASON_UNKNOWN = "DISPOSE_REASON_UNKNOWN"'))
         .pipe(gulpReplace('DISPOSE_REASON_ERROR = 1', 'DISPOSE_REASON_ERROR = "DISPOSE_REASON_ERROR"'))
         .pipe(gulpReplace('DISPOSE_REASON_CALL_DOES_NOT_EXISTS = 2', 'DISPOSE_REASON_CALL_DOES_NOT_EXISTS = "DISPOSE_REASON_CALL_DOES_NOT_EXISTS"'))
         .pipe(gulpReplace('DISPOSE_REASON_PICKED_UP = 3', 'DISPOSE_REASON_PICKED_UP = "DISPOSE_REASON_PICKED_UP"'))
@@ -84,14 +87,16 @@ gulp.task ('grpc-replace-enum', ['grpc-generate'], function () {
         .pipe(gulpReplace('DISPOSE_REASON_BUSY = 7', 'DISPOSE_REASON_BUSY = "DISPOSE_REASON_BUSY"'))
         .pipe(gulpReplace('DISPOSE_REASON_REJECTED = 8', 'DISPOSE_REASON_REJECTED = "DISPOSE_REASON_REJECTED"'))
 
+        .pipe(gulpReplace('HANGUP_REASON_UNKNOWN = 0', 'HANGUP_REASON_UNKNOWN = "HANGUP_REASON_UNKNOWN"'))
         .pipe(gulpReplace('HANGUP_REASON_ERROR = 1', 'HANGUP_REASON_ERROR = "HANGUP_REASON_ERROR"'))
         .pipe(gulpReplace('HANGUP_REASON_NORMAL = 2', 'HANGUP_REASON_NORMAL = "HANGUP_REASON_NORMAL"'))
         .pipe(gulpReplace('HANGUP_REASON_BUSY = 3', 'HANGUP_REASON_BUSY = "HANGUP_REASON_BUSY"'))
         .pipe(gulpReplace('HANGUP_REASON_REJECTED = 4', 'HANGUP_REASON_REJECTED = "HANGUP_REASON_REJECTED"'))
 
-        .pipe(gulpReplace('ICE_SETTINGS_RELAY = 1', 'ICE_SETTINGS_RELAY = "ICE_SETTINGS_RELAY"'))
-        .pipe(gulpReplace('ICE_SETTINGS_ALL = 2', 'ICE_SETTINGS_ALL = "ICE_SETTINGS_ALL"'))
-        .pipe(gulpReplace('ICE_SETTINGS_NONE = 3', 'ICE_SETTINGS_NONE = "ICE_SETTINGS_NONE"'))
+        .pipe(gulpReplace('ICE_SETTINGS_UNKNOWN = 0', 'ICE_SETTINGS_UNKNOWN = "ICE_SETTINGS_UNKNOWN"'))
+        .pipe(gulpReplace('ICE_SETTINGS_NONE = 1', 'ICE_SETTINGS_NONE = "ICE_SETTINGS_NONE"'))
+        .pipe(gulpReplace('ICE_SETTINGS_RELAY = 2', 'ICE_SETTINGS_RELAY = "ICE_SETTINGS_RELAY"'))
+        .pipe(gulpReplace('ICE_SETTINGS_ALL = 3', 'ICE_SETTINGS_ALL = "ICE_SETTINGS_ALL"'))
    
         .pipe(gulp.dest('./generated/'));    
 });
