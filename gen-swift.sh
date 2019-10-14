6df4bc43f914b743c99c595c2c9efbbb46a8db42
Nikita Gusakov <gusnkt@gmail.com>
2019-10-14 11:02:16 +0300

    feat: initial commit

---
+++ gen-swift.sh
@@ -0,0 +1,11 @@
+rm -f -rf ./Sources/DialogCalls-GRPC/
+
+mkdir -p Sources/DialogCalls-GRPC 
+
+./swiftDependency/protoCompiler/protoc server.proto \
+--plugin=protoc-gen-swift=./swiftDependency/Proto/protoc-gen-swift \
+--plugin=protoc-gen-swiftgrpc=./swiftDependency/GRPC/protoc-gen-swiftgrpc \
+--swift_out=FileNaming=DropPath,Visibility=Public:Sources/DialogCalls-GRPC \
+--swiftgrpc_out=Client=true,Server=false,TestStubs=true,FileNaming=DropPath,Visibility=Public:Sources/DialogCalls-GRPC \
+-I ./swiftDependency/protoCompiler/ \
+-I .
